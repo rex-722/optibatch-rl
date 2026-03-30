aeb01f6bc66003e45567bb5b079cc2d1221f133e
rex-722 <rajthakur951000@gmail.com>
2026-03-31 01:56:06 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,13 @@
 import sys
 import os
+import uvicorn
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server import app
+
+#main() function 
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=7860)
+  
+if __name__ == "__main__":
+    main()
